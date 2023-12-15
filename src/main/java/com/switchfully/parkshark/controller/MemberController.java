609fb0c7dd07bd747de8747f87918cc234100f4f
Jussi Ccoyllo <jussi.ccoyllo@smals.be>
2023-12-15 15:16:01 +0100

    add authenticate

--- src/main/java/com/switchfully/parkshark/controller/MemberController.java
+++ src/main/java/com/switchfully/parkshark/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.switchfully.parkshark.controller;
 
 import com.switchfully.parkshark.dto.CreateMemberDto;
 import com.switchfully.parkshark.dto.MemberDto;
+import com.switchfully.parkshark.service.AdminService;
 import com.switchfully.parkshark.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -15,9 +16,11 @@ import static org.springframework.http.HttpStatus.CREATED;
 public class MemberController {
 
     private MemberService memberService;
+    private AdminService adminService;
 
-    public MemberController(MemberService memberService) {
+    public MemberController(MemberService memberService, AdminService adminService) {
         this.memberService = memberService;
+        this.adminService = adminService;
     }
 
     @PostMapping(consumes = "application/json")
@@ -27,12 +30,14 @@ public class MemberController {
     }
 
     @GetMapping
-    public List<MemberDto> getAllMembers(){
+    public List<MemberDto> getAllMembers(@RequestHeader String email, @RequestHeader String password){
+        adminService.authenticate(email, password);
         return memberService.getAllMembers();
     }
 
     @GetMapping("/{id}")
-    public MemberDto getMemberById(@PathVariable Long id){
+    public MemberDto getMemberById(@RequestHeader String email, @RequestHeader String password, @PathVariable Long id){
+        adminService.authenticate(email, password);
         return memberService.getById(id);
     }
 
