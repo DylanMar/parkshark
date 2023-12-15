13b42986317e10fd7367848a9ee085939edc5786
Karel Van Geerdeghom <karel.van.geerdeghom@gmail.com>
2023-12-15 14:32:25 +0100

    Add level type to test db

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,3 +1,5 @@
 INSERT INTO level (id, monthly_cost, allocation_reduction, maximum_time)
 VALUES
-    (1, 10, 10, 10);
+    (1, 10, 10, 10, "BRONZE"),
+    (2, 10, 10, 10, "SILVER"),
+    (3, 10, 10, 10, "GOLD");
