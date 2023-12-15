9f8e171e4bc9c238a76507979e551152583d7787
Jussi Ccoyllo <jussi.ccoyllo@smals.be>
2023-12-15 15:53:35 +0100

    add member controller integration test

---
+++ src/test/java/com/switchfully/parkshark/controller/MemberControllerIntegrationTest.java
@@ -0,0 +1,48 @@
+package com.switchfully.parkshark.controller;
+
+import com.switchfully.parkshark.dto.MemberDto;
+import io.restassured.RestAssured;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.List;
+
+import static io.restassured.http.ContentType.JSON;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@Sql("/data.sql")
+public class MemberControllerIntegrationTest {
+    @LocalServerPort
+    private int port;
+
+    @Test
+    void givenEmailAndPassword_whenGetAllMembers_thenGetAllMembers(){
+        //GIVEN
+        String email = "admin@parkshark.com";
+        String password = "admin";
+
+        //WHEN
+        List<MemberDto> actual =
+                RestAssured
+                        .given()
+                        .contentType(JSON)
+                        .header("email", email)
+                        .header("password", password)
+                        .when()
+                        .port(port)
+                        .get("/members")
+                        .then()
+                        .assertThat()
+                        .statusCode(HttpStatus.OK.value())
+                        .extract()
+                        .body()
+                        .jsonPath()
+                        .getList(".", MemberDto.class);
+        //THEN
+        Assertions.assertThat(actual).hasSize(0);
+    }
+}
--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,5 +1,9 @@
-INSERT INTO level (id, monthly_cost, allocation_reduction, maximum_time)
+INSERT INTO level (id, monthly_cost, allocation_reduction, maximum_time,level_type)
 VALUES
-    (1, 10, 10, 10, "BRONZE"),
-    (2, 10, 10, 10, "SILVER"),
-    (3, 10, 10, 10, "GOLD");
+    (1, 10, 10, 10, 'BRONZE'),
+    (2, 10, 10, 10, 'SILVER'),
+    (3, 10, 10, 10, 'GOLD');
+
+INSERT INTO admin (id, email, password)
+VALUES
+(1, 'admin@parkshark.com', '$2a$12$r3fLIlWCAzvA6qgIqf.60uDPFn00QwpS6ba28LNvSXBYF0eHbVDAW');
